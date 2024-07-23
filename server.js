8436e3b1a20633b654f8f43d310fd760b3126706
dream585 <gtran@southrivermtg.com>
2024-07-24 01:21:49 +0700

    path library added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 const port = process.env.PORT || 4000;
 
